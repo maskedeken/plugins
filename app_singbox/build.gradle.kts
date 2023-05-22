66249602c4c6ba7fd3cc7f7061400a9b17658cfd
Eken Chan <maskedeken@yahoo.com>
2023-05-22 10:14:33 +0800

    update sing-box to 1.2.7

--- app_singbox/build.gradle.kts
+++ app_singbox/build.gradle.kts
@@ -8,8 +8,8 @@ setupAll()
 android {
     defaultConfig {
         applicationId = "moe.matsuri.plugin.singbox"
-        versionCode = 13
-        versionName = "v1.2.6"
+        versionCode = 14
+        versionName = "v1.2.7"
         splits.abi {
             reset()
             include("arm64-v8a", "armeabi-v7a")
--- download.sh
+++ download.sh
@@ -41,7 +41,7 @@ download_xray() {
 }
 
 download_singbox() {
-  VERSION="1.2.6"
+  VERSION="1.2.7"
   mkdir_libs "app_singbox/libs"
   dl_and_chmod arm64-v8a/libsingbox.so "https://github.com/maskedeken/sing-box/releases/download/v$VERSION/sing-box-android-arm64"
   dl_and_chmod armeabi-v7a/libsingbox.so "https://github.com/maskedeken/sing-box/releases/download/v$VERSION/sing-box-android-arm"
