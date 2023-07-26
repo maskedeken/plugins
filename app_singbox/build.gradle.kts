cefb70f0f8c6ae8706de01ea8d0e847d9229e495
Eken Chan <maskedeken@yahoo.com>
2023-07-26 10:31:48 +0800

    update sing-box plugin

--- app_singbox/build.gradle.kts
+++ app_singbox/build.gradle.kts
@@ -8,8 +8,8 @@ setupAll()
 android {
     defaultConfig {
         applicationId = "moe.matsuri.plugin.singbox"
-        versionCode = 17
-        versionName = "v1.3"
+        versionCode = 18
+        versionName = "v1.3.4"
         splits.abi {
             reset()
             include("arm64-v8a", "armeabi-v7a")
